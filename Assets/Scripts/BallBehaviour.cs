4b072ce7bf318cc4d512abaa186573b21671c16a
oj_lappi <olappi@abo.fi>
2018-01-18 13:25:47 +0200

    Cleanup

---
+++ Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+struct influencer{
+        public Vector2 position;
+        public float influence;
+    };
+
+public class BallBehaviour : MonoBehaviour {
+    
+    private influencer[] influencers = {new influencer(), new influencer() };
+    public bool powered = true;
+    Vector2 target;
+    Rigidbody2D body;
+    Vector2 integral;
+    Vector2 derivative;
+    Vector2 error;
+    Vector2 last_error;
+    GameObject[] players;
+    const float integrating_dampener = 0.9f;
+    const float general_dampener = 1f;
+    const float proportional_c = 6.5f;
+    const float integrating_c = 1.4f;
+    const float derivating_c = 1.2f;
+
+    Vector2 maxVelocity = new Vector2(200,200);
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+    }
+	
+    void PID(influencer[] influencers)
+    {
+        Vector2 velocity;
+
+        target = calculate_target(influencers);
+        Debug.DrawLine(new Vector3(target.x-10,target.y-10,0), new Vector3(target.x+10, target.y+10, 0), Color.red);
+
+        last_error = error;
+        error = target - body.position;
+        integral *= integrating_dampener;
+        integral += last_error;
+        derivative = error - last_error;
+
+        //PID controller below
+        velocity = proportional_c * error + integrating_c * integral + derivating_c * derivative;
+        velocity *= general_dampener;
+        if(Math.Abs(velocity.x) > maxVelocity.x)
+        {
+            velocity.x = velocity.x > 0 ? maxVelocity.x : -maxVelocity.x;
+        }
+        if (Math.Abs(velocity.y) > maxVelocity.y)
+        {
+            velocity.y = velocity.y > 0 ? maxVelocity.y : -maxVelocity.y;
+        }
+        /*
+        Debug.Log(
+            String.Format(
+            "target: x:{0} y:{1} \n" +
+            "error: x:{2} y:{3} \n" +
+            "velocity: x:{4} y:{5} \n",
+            target.x,target.y,
+            error.x, error.y,
+            velocity.x,velocity.y)
+            );
+            */
+        body.velocity = velocity;
+
+        
+    }
+
+    private Vector2 calculate_target(influencer[] influencers)
+    {
+        //This only supports two influencers for now
+        //TODO: n influencer targets? i.e 3 players and up
+
+        //linear interpolation to get a target in between the two influencers corresponding to their influence
+        return           influencers[0].position 
+                         + (
+                                influencers[0].influence
+                                    / 
+                                (influencers[0].influence+ influencers[1].influence) 
+                            )
+                            * (influencers[1].position - influencers[0].position); 
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        influencers[0].position = players[0].GetComponent<Rigidbody2D>().position;
+        influencers[1].position = players[1].GetComponent<Rigidbody2D>().position;
+        influencers[0].influence = players[0].GetComponent<PlayerBehaviour>().influence;
+        influencers[1].influence = players[1].GetComponent<PlayerBehaviour>().influence;
+        if (influencers.Length > 2)
+            return;//Add support for three players?
+        PID(influencers);
+
+        
+
+
+	}
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+
+    public int influence = 1;
+    int concentration = 100; //Depletes while using power
+    private string movement_axis_hor, movement_axis_vert;
+    public int player_id = 1;
+    float horInput;
+    float vertInput;
+
+    Rigidbody2D body;
+    GameObject ball;
+
+    // Use this for initialization
+    void Start () {
+
+        influence = 1;
+        // The axes names are based on player number.
+        movement_axis_hor = "Horizontal" +player_id;
+        movement_axis_vert = "Vertical" + player_id;
+
+        body = GetComponent<Rigidbody2D>();
+        ball = GameObject.FindGameObjectWithTag("Ball");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        horInput =Input.GetAxis(movement_axis_hor)*10;
+        vertInput = Input.GetAxis(movement_axis_vert)*10;
+
+        Debug.Log(
+        String.Format(
+            "playerId{3}\n"+
+        "influence: {0}\n"+
+        "position: x:{1},y:{2}",
+           
+        influence,
+        body.position.x, body.position.y, player_id)
+    );
+        if (vertInput < 0)
+        {
+            influence = concentration;
+            concentration -= 2;
+            vertInput = 0;
+        }
+        else 
+        {
+            influence = influence / 2;
+            concentration += 2;
+        }
+        influence = influence<1?1:influence;
+        concentration = concentration < 0?0: (concentration > 100?concentration:100);
+        body.velocity =  new Vector2(horInput*10,vertInput*10);
+        
+
+    }
+}
